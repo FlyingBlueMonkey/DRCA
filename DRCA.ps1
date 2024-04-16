8da5d8cbcc37d051645ba1510e036c33887408e0
Matt Egen <matt@chromeweb.com>
2024-04-16 12:14:29 -0700

    Change referenceGenerator.ps1 to only output properties and changed comparison to check each property value.  This is the brute force route

--- DRCA.ps1
+++ DRCA.ps1
@@ -19,20 +19,39 @@ Write-Host $referenceConfigurationObject.GetType()
 
 # Compare the local configuration to the reference configuration
 $comparisonResult = Compare-Object -ReferenceObject $referenceConfigurationObject -DifferenceObject $localConfigurationObject -IncludeEqual
+# Compare the objects and list changes
+$differences = @()
 
-# Generate HTML report
-$htmlReport = "<html><head><style>table, th, td {border: 1px solid black; border-collapse: collapse;} th, td {padding: 10px;} th {text-align: left;}</style></head><body><h1>Comparison Results</h1>"
-if ($null -eq $comparisonResult) {
-    $htmlReport += "<p>The local configuration and reference configuration are identical.</p>"
-} else {
-    $htmlReport += "<table><tr><th>Property</th><th>Reference Value</th><th>Local Value</th><th>Side Indicator</th></tr>"
-    foreach ($result in $comparisonResult) {
-        $htmlReport += "<tr><td>$($result.Property)</td><td>$($result.ReferenceObject | Select-Object -ExpandProperty $result.Property)</td><td>$($result.DifferenceObject | Select-Object -ExpandProperty $result.Property)</td><td>$($result.SideIndicator)</td></tr>"
+foreach ($prop in $localConfigurationObject.PSObject.Properties.Name) {
+    $currentValue = $currentPreferences.$prop
+    $referenceValue = $referenceConfigurationObject.$prop
+
+    if ($currentValue -ne $referenceValue) {
+        $diff = New-Object -TypeName PSObject -Property @{
+            Property = $prop
+            ReferenceValue = $referenceValue
+            CurrentValue = $currentValue
+        }
+        $differences += $diff
     }
-    $htmlReport += "</table>"
 }
 
-$htmlReport += "</body></html>"
+# Display differences
+$differences | Format-Table -AutoSize
+
+# Generate HTML report
+#$htmlReport = "<html><head><style>table, th, td {border: 1px solid black; border-collapse: collapse;} th, td {padding: 10px;} th {text-align: left;}</style></head><body><h1>Comparison Results</h1>"
+#if ($null -eq $comparisonResult) {
+#    $htmlReport += "<p>The local configuration and reference configuration are identical.</p>"
+#} else {
+#    $htmlReport += "<table><tr><th>Property</th><th>Reference Value</th><th>Local Value</th><th>Side Indicator</th></tr>"
+#    foreach ($result in $comparisonResult) {
+#        $htmlReport += "<tr><td>$($result.Property)</td><td>$($result.ReferenceObject | Select-Object -ExpandProperty $result.Property)</td><td>$($result.DifferenceObject | Select-Object -ExpandProperty $result.Property)</td><td>$($result.SideIndicator)</td></tr>"
+#    }
+#    $htmlReport += "</table>"
+#}
+
+#$htmlReport += "</body></html>"
 
 # Output the HTML report to a file
-$htmlReport | Out-File ".\DRCA.html"
+#$htmlReport | Out-File ".\DRCA.html"
